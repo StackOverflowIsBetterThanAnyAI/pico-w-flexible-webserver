b99d22e818b090d9b91bbf41d9b5646fac4e12ab
Michael <140268904+StackOverflowIsBetterThanAnyAI@users.noreply.github.com>
2024-07-10 18:22:06 +0200

    Create env.py

---
+++ lib/env.py
@@ -0,0 +1,17 @@
+def xor_encrypt_decrypt(data, key):
+    return bytearray([b ^ key for b in data])
+
+# access data
+ssid = b"123-456-789-0"
+password = b"****************"
+key = 0x55  # XOR key
+
+# encrypting
+encrypted_ssid = xor_encrypt_decrypt(ssid, key)
+encrypted_password = xor_encrypt_decrypt(password, key)
+
+# save the encrypted data in another file
+with open("/lib/env_encrypted.py", "w") as env_file:
+    env_file.write(f"ssid = {encrypted_ssid}\n")
+    env_file.write(f"password = {encrypted_password}\n")
+    env_file.write(f"key = {key}\n")
